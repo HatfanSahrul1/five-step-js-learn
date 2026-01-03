b398fd9385533c9ac76da53483d95f69a7f29586
HatfanSahrul1 <hatfansahrul1@gmail.com>
2026-01-03 13:34:34 +0700

    feat : completing code

--- cli/main.js
+++ cli/main.js
@@ -5,28 +5,42 @@ const rl = readline.createInterface({
     output : process.stdout,
 });
 
-const ops = ([...numbers], opsType) => {
-
+const operation = ([...numbers], opsType) => {
     [first, ...rest] = numbers;
-
     switch (opsType) {
         case "tambah": return rest.reduce((acc, curr) => acc + curr, first); break;
         case "kurang": return rest.reduce((acc, curr) => acc - curr, first); break;
         case "kali": return rest.reduce((acc, curr) => acc * curr, first); break;
         case "bagi": return rest.reduce((acc, curr) => acc / curr, first); break;
-
-        default:
-            break;
+        default: break;
     }
 };
 
-
 function ask(question){
+    console.log(`Command List : \n- 'tambah'\n- 'kurang'\n- 'kali'\n- 'bagi'\n`);
+    console.log(`contoh : tambah 2 4 5 2 3 22 4.6\n`);
+
+    const opsList = ["tambah", "kurang", "kali", "bagi"];
+    
     rl.question(question, (answer)=>{
+        let [opsType, ...list]= answer.split(" ");
+        let total;
+
+        opsType = opsType.toString().toLowerCase();
+        list = list.map((number) => Number(number));
         
+        if(opsList.includes(opsType)){
+            total = operation(list, opsType);
+            console.log(`==================================`);
+            console.log(`Hasilnya adalah : ${total}\n`);
+            console.log(`==================================`);
+        }else{
+            console.log(`Command Salah [${opsType}]\n`);
+        }
     });
+
+    ask(question);
 }
 
-let test = ops([1, 3, 2, 2], "bagi");
-
-console.log(test);
+console.log("SELAMAT DATANG DI KALKULATOR CLI\n");
+ask("Masukkan perintah : ");
